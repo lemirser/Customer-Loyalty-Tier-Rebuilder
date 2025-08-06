12c895bf1441640a9e6115b63158f11ce3756126
lemirser <serzoleandro@gmail.com>
2025-08-06 17:52:07 +0800

    Create a separate logging configuration file

---
+++ helpers/logger_config.py
@@ -0,0 +1,14 @@
+import os
+import logging
+from dotenv import load_dotenv
+
+def setup_logging():
+    load_dotenv()
+    log_level = os.getenv("LOG_LEVEL", "INFO").upper()
+    log_format = os.getenv("LOG_FORMAT", "%(levelname)s - %(message)s")
+    log_file = os.getenv("LOG_FILE", "logs.log")
+
+    if log_file:
+        logging.basicConfig(filename=log_file, level=log_level, format=log_format)
+    else:
+        logging.basicConfig(level=log_level, format=log_format)

c39cea82d143172b137e710b1ecbb49a42b366ca
lemirser <serzoleandro@gmail.com>
2025-08-10 02:57:24 +0800

    Function to check the parameters

--- helpers/email_notifier.py
+++ helpers/email_notifier.py
@@ -40,3 +40,38 @@ def email_check(email_address) -> str:
         raise
 
     return email
+
+
+def sanitize_parameters(
+    receiver_emails,
+    subject="Loyalty Program Update",
+    email_provider="gmail",
+) -> tuple:
+    """Sanitize any invalid characters.
+
+    Args:
+        receiver_emails (str): customer email address
+        subject (str, optional): Email Subject line. Defaults to "Loyalty Program Update".
+        email_provider (str, optional): Email provider for the sender. Defaults to "gmail".
+
+    Returns:
+        tuple: Returns the sanitized parameters
+    """
+    logger.info("Sanitizing parameters.")
+
+    if len(subject) > 30:
+        logging.error("Subject line should only contain 30 or less characters.")
+        raise
+
+    if email_provider.strip().lower() not in ["gmail", "yahoo", "outlook"]:
+        logging.error(f"Invalid email provider: {email_provider}")
+        raise
+
+    clean_subject = re.sub(r"[^a-zA-Z0-9]", "", subject)
+    clean_receiver_emails = email_check(receiver_emails)
+
+    result = (email_provider, clean_receiver_emails, clean_subject)
+
+    logger.info("Done checking the parameters.")
+
+    return result

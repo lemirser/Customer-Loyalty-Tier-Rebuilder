889ce4a807a9c433132a88554fd3d492d5323379
lemirser <serzoleandro@gmail.com>
2025-08-12 05:56:52 +0800

    Read CSV extract

--- helpers/transform_load_pyspark.py
+++ helpers/transform_load_pyspark.py
@@ -1,4 +1,12 @@
 from pyspark.sql import SparkSession
+from pyspark.sql.types import (
+    DoubleType,
+    IntegerType,
+    StringType,
+    DateType,
+    StructField,
+    StructType,
+)
 
 
 def init_spark(app_name: str = "CustomerLoyaltyTierApp"):
@@ -20,9 +28,37 @@ def init_spark(app_name: str = "CustomerLoyaltyTierApp"):
     return spark
 
 
+def read_file():
+    """Read the extracted file
+
+    Returns:
+        DataFrame: Spark DataFrame
+    """
+    schema = StructType(
+        [
+            StructField("transaction_id", IntegerType(), False),
+            StructField("customer_id", IntegerType(), False),
+            StructField("transaction_date", DateType(), False),
+            StructField("amount", DoubleType(), False),
+            StructField("first_name", StringType(), False),
+            StructField("last_name", StringType(), False),
+            StructField("email", StringType(), False),
+            StructField("registration_date", DateType(), False),
+        ]
+    )
+
+    file_path = "extract/upload/raw_transactions_UPLOAD.csv"
+
+    df = (
+        spark.read.format("csv").option("header", "true").schema(schema).load(file_path)
+    )
+
+    return df
+
+
 if __name__ == "__main__":
     spark = init_spark()
 
-    print("Spark Session")
+    print(read_file().limit(10).show())
 
     spark.stop()

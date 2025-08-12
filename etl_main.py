1ab9eab0eeb9a896c6a9cb300790b22f1ad1fb43
lemirser <serzoleandro@gmail.com>
2025-08-13 02:05:33 +0800

    Add emailing function

--- etl_main.py
+++ etl_main.py
@@ -4,7 +4,15 @@ from helpers.logger_config import setup_logging
 from helpers.file_uploader import file_uploader
 from helpers.db_connection import initialize_database
 from helpers.etl_utils import raw_to_csv, backup_file
-from helpers.transform_load_pyspark import init_spark, read_file, group_data, tier
+from helpers.transform_load_pyspark import (
+    init_spark,
+    read_file,
+    group_data,
+    tier,
+    process_user_email,
+)
+from helpers.email_notifier import html_template, send_email
+import os
 
 # Load environment variables
 load_dotenv()
@@ -37,6 +45,25 @@ if __name__ == "__main__":
     spark = init_spark()
     df = read_file(spark)
     df_grouped = group_data(df)
-    tier(df_grouped).limit(10).show()
+    df_tier = tier(df_grouped)
+    user_details = process_user_email(df_tier)
+
+    # Emailing
+    for user in user_details.items():
+        main_template = html_template(
+            current_tier=user[1]["tier"],
+            old_tier="Platinum",  # Testing
+            customer_name=f"{user[1]["fname"]} {user[1]["lname"]}",
+            customer_email=user[0],
+            sum_amount=user[1]["total_amount"],
+            transaction_count=user[1]["transaction_count"],
+        )
+
+        send_email(
+            sender_email=os.getenv("EMAIL_USER"),
+            sender_password=os.getenv("EMAIL_PASS"),
+            receiver_emails=user[0],
+            html_content=main_template,
+        )
 
     logger.info("=== Process end ===")
